f77e1b4b96a36051e4572be34097da62b09dad25
SeanyCarol <seany@ice.ufjf.br>
2024-02-06 11:01:18 -0300

    Create VerifyHasCertificationDTO

---
+++ src/main/java/com/seanycarol/certification_nlw/modules/students/dtos/VerifyHasCertificationDTO.java
@@ -0,0 +1,14 @@
+package com.seanycarol.certification_nlw.modules.students.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VerifyHasCertificationDTO {
+
+    private String email;
+    private String technology;
+}
