1ffa9000e48f71134fcbfb9cd34aa746adf96317
SeanyCarol <seany@ice.ufjf.br>
2024-02-08 14:43:36 -0300

    Create student certification answer

---
+++ src/main/java/com/seanycarol/certification_nlw/modules/students/dtos/QuestionAnswerDTO.java
@@ -0,0 +1,19 @@
+package com.seanycarol.certification_nlw.modules.students.dtos;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionAnswerDTO {
+
+    private UUID questionID;
+    private UUID alternativeID;
+    private boolean isCorrect;
+}
---
+++ src/main/java/com/seanycarol/certification_nlw/modules/students/dtos/StudentCertificationAnswerDTO.java
@@ -0,0 +1,19 @@
+package com.seanycarol.certification_nlw.modules.students.dtos;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentCertificationAnswerDTO {
+
+    private String email;
+    private String technology;
+    private List<QuestionAnswerDTO> questionsAnswers;
+}
